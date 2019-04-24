c216affbf3630ba5473dbf623c3e8c9d8f5fb49f
hekenderi <hemprasadk@gmail.com>
2019-04-24 18:06:05 -0400

    intial Upload

---
+++ src/app/WaringAlert/warningalert.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './warningalert.component.html',
+  styleUrls: ['./warningalert.componenet.css']
+
+  }
+)
+
+export class WarnAlertComponent {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import {WarnAlertComponent} from './WaringAlert/warningalert.component';
+import { SuccessesComponent } from './successes/successes.component';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarnAlertComponent,
+    SuccessesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
